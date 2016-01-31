937a293c4ac054fdaaca51f19c54436c12a38d70
janbodnar <jan.bodnar@gmail.com>
2016-01-31 13:49:30 +0100

    Update win_console.c

--- main/win_console.c
+++ main/win_console.c
@@ -1,1 +1,17 @@
+#include <windows.h>
+#include <wchar.h>
 
+int wmain(int argc, wchar_t **argv) {
+
+    PDWORD cChars = NULL;
+    HANDLE std = GetStdHandle(STD_OUTPUT_HANDLE);   
+    
+    if (std == INVALID_HANDLE_VALUE) {
+        wprintf(L"Cannot retrieve standard output handle\n (%d)", 
+            GetLastError());
+    }
+ 
+    WriteConsoleW(std, argv[1], wcslen(argv[1]), cChars, NULL);
+ 
+    return EXIT_SUCCESS;
+}

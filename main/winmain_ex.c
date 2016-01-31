d07744be62cb5cb72aeebf580df83b0481a2b476
janbodnar <jan.bodnar@gmail.com>
2016-01-31 13:51:52 +0100

    Create winmain_ex.c

---
+++ main/winmain_ex.c
@@ -0,0 +1,9 @@
+#include <windows.h>
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, 
+               PWSTR szCmdLine, int CmdShow) {
+               
+    MessageBoxW(NULL, szCmdLine, L"Title", MB_OK);
+
+    return EXIT_SUCCESS;
+}

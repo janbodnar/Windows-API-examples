5b8c35be77e0f64afad9d92bde91c165d49c242d
janbodnar <jan.bodnar@gmail.com>
2016-01-31 14:00:59 +0100

    Create utc_time.c

---
+++ datetime/utc_time.c
@@ -0,0 +1,14 @@
+#include <windows.h>
+#include <wchar.h>
+
+int wmain(void) {
+
+    SYSTEMTIME st = {0};
+  
+    GetSystemTime(&st);
+  
+    wprintf(L"The system time is: %02d:%02d:%02d\n", 
+        st.wHour, st.wMinute, st.wSecond);
+
+    return 0;
+}
